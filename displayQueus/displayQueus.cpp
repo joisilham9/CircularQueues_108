f6b596edce9ce933caf6fcd07b8fd63bdb520d0f
joisilham9 <127075874+joisilham9@users.noreply.github.com>
2023-06-22 15:32:55 +0700

    membuat void display

--- displayQueus/displayQueus.cpp
+++ displayQueus/displayQueus.cpp
@@ -21,7 +21,7 @@ public:
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
 			cout << "\nQueue overflow \n";
 			return;
-			}
+		}
 
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
@@ -52,7 +52,11 @@ public:
 			REAR = -1;
 		}
 		else {
-			FRONT = FRONT + 1;
+			//jika elemen yang dihapus berada di posisi terakhir array ,kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
 		}
 	}
 
@@ -62,7 +66,26 @@ public:
 
 		//cek apakah antrian kosong
 		if (FRONT == -1) {
+			cout << "queue is empty\n";
+			return;
+		}
 
+		cout << "\nElements in the queue are...\n";
+
+		//jika FRONT_position <= REAR_position iterasi dari FRONT hingga REAR
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position iterasi dari FRONT hingga akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
 		}
 	}
 };
